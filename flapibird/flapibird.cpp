bf4c232f80156816deab4f28375e73cdb3b57598
ruslanNasibov <nasibovruslanwork@gmail.com>
2024-02-27 23:02:28 +0200

    continueworking

--- flapibird/flapibird.cpp
+++ flapibird/flapibird.cpp
@@ -1,37 +1,70 @@
 ﻿#include<iostream>
 #include<Windows.h>
 #include<conio.h>
+#include<thread>
+#include<chrono>
 using namespace std;
-
+int columnX = 10;
+int columnY = 3;
 bool gameOver = 0;
 int birdY = 10;
-enum DIRECTION { STOP = 0, JUMP };
-DIRECTION dir;
 void input() {
 	if (_kbhit()) {
-		if (_getch() == 'j')
+		if (_getch() == ' ')
 		{
-			birdY++;
-			Sleep(1);
-			birdY++;
+			birdY--;
 		}
 	}
 }
+void birdLogic() {
+	while (!gameOver) {
+		this_thread::sleep_for(chrono::milliseconds(600));
+		birdY++;
+		this_thread::sleep_for(chrono::milliseconds(200));
+		birdY++;
+	}
+}
 void logic() {
+	
 
+	if (birdY >= 20 || birdY <= 0) {
+		gameOver = true;
+	}
+	
 }
 void draw() {
 	system("cls");
-	for (int i = 0; i <= 20; i++)
+	for (int y = 0; y < 20; y++)
 	{
+		if (y == birdY)
+		{
+			cout << "O";
+		}
+		for (int x = 0; x < 20; x++)
+		{
+			if (x == columnX)
+			{
+				cout << '#';
+			}
+			else {
+				if (y != birdY) {
+					cout << ' ';
+				}
+			}
+		}
 		cout << endl;
 	}
-	cout << "####################" << endl;
+	
+	cout << "###################################################################################################################################################" << endl;
 }
 int main()
 {
+	thread decreaseThread(birdLogic); // Запускаем поток birdLogic
 	while (!gameOver) {
 		draw();
+		input();
+		logic();
 	}
+	decreaseThread.join(); // Дождемся завершения потока перед завершением программы
 	return 0;
 }
